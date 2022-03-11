446e03340a04cbe644bc4855ca691a062a8e4e6c
mackdani <98480193+mackdani@users.noreply.github.com>
2022-03-10 22:17:57 -0500

    Add files via upload

---
+++ assignment2-cis-297-winter-2022-mackdani-main/TurtleGraphics/Pen.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurtleGraphics
+{
+    public class Pen
+    {
+        private PenActions pen_actions;
+
+        public Pen()
+        {
+            pen_actions = PenActions.Up;
+        }
+        public enum PenActions
+        {
+            Up = 1, 
+            Down = 2
+        };
+
+        public PenActions pen_moving
+        {
+            get { return pen_actions; }
+            set
+            {
+                switch ((int)value)
+                {
+                    case 1:
+                        pen_actions = PenActions.Up;
+                        break;
+                    case 2:
+                        pen_actions = PenActions.Down;
+                        break;
+                    default:
+                        GameInstructions.PenActionError();
+                        break;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return String.Format("Pen is {0}", pen_moving == PenActions.Up ? "Up" : "Down");
+        }
+    }
+}
---
+++ assignment2-cis-297-winter-2022-mackdani-main/TurtleGraphics/Turtle.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurtleGraphics
+{
+    public class Turtle
+    {
+        public Turtle()
+        {
+            TurtleSymbol = 'N';
+            PositionX = 0;
+            PositionY = 0;
+            Move = 0;
+        }
+        public char TurtleSymbol { get; set; }
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+        public int Move { get; set; }
+        public void Movement(AllMovements.Directions moving, int spaces, Pen.PenActions pen)
+        {
+            if (CheckingtheMoves(moving, spaces))
+            {
+                var toDraw = (pen == Pen.PenActions.Down);
+                switch (moving)
+                {
+                    case AllMovements.Directions.North:
+                        if (toDraw)
+                        {
+                            GraphicBoard.UpdateGraphicBoardX(PositionX, -1, spaces, PositionY);
+                        }
+                        PositionX -= spaces;
+                        break;
+                    case AllMovements.Directions.South:
+                        if (toDraw)
+                        {
+                            GraphicBoard.UpdateGraphicBoardX(PositionX, +1, spaces, PositionY);
+                        }
+                        PositionX += spaces;
+                        break;
+                    case AllMovements.Directions.West:
+                        if (toDraw)
+                        {
+                            GraphicBoard.UpdateGraphicBoardY(PositionY, -1, spaces, PositionX);
+                        }
+                        PositionY -= spaces;
+                        break;
+                    case AllMovements.Directions.East:
+                        if (toDraw)
+                        {
+                            GraphicBoard.UpdateGraphicBoardY(PositionY, +1, spaces, PositionX);
+                        }
+                        PositionY += spaces;
+                        break;
+                }
+            }
+        }
+        private bool CheckingtheMoves(AllMovements.Directions movements, int spaces)
+        {
+            if(movements == AllMovements.Directions.North && (PositionX - spaces) < 0)
+            {
+                GameInstructions.MoveError(movements, PositionX);
+                return false;
+            }
+            else if (movements == AllMovements.Directions.East && (PositionY + spaces) > GraphicBoard.GraphicBoardSize)
+            {
+                GameInstructions.MoveError(movements, GraphicBoard.GraphicBoardSize - PositionY - 1);
+                return false;
+            }
+            else if (movements == AllMovements.Directions.South && (PositionX + spaces) > GraphicBoard.GraphicBoardSize)
+            {
+                GameInstructions.MoveError(movements, GraphicBoard.GraphicBoardSize - PositionX - 1);
+                return false;
+            }
+            else if (movements == AllMovements.Directions.West && (PositionY - spaces) < 0)
+            {
+                GameInstructions.MoveError(movements, PositionY);
+                return false;
+            }
+            return true;
+        }
+    }
+}
